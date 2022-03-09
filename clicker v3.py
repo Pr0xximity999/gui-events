7378fd835f5f85fc355057f45ca3a3b74bc2135f
Thomas Koman <99066789@mydavinci.nl>
2022-03-09 10:24:19 +0100

    clicker v3

--- clicker v3.py
+++ clicker v3.py
@@ -28,14 +28,12 @@ def update(event=''):
 def yellow(event=''):
     window.configure(bg='yellow')
 
-def triple(event=''):
-    global num
-    num *= 3
-    update()
-
-def deTriple(event=''):
-    global num
-    num /= 3
+def tripleOrNah(event=''):
+    global num, UpLastPressed, DownLastPressed
+    if UpLastPressed:
+        num *= 3
+    if DownLastPressed:
+        num /= 3
     update()
 
 
@@ -68,11 +66,8 @@ downBtn = Button(text="down", width=32, height=1, command=decrement)
 downBtn.pack() 
 downBtn.place(relx=0.5, rely=0.8, anchor=CENTER)  
 
-#Binds the button double click events
-upBtn.bind('<Double-Button-1>', triple)
-downBtn.bind('<Double-Button-1>', deTriple)
-
-#When the mouse hovers over the label, fire the yellow function and else, just the regular
+#Label events
+text.bind('<Double-Button-1>', tripleOrNah)
 text.bind('<Enter>', yellow)
 text.bind('<Leave>', update)
 
